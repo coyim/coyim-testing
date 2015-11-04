505cf5d21d5a88d7157dc864ddb6dfaf582ff07c
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-11-04 13:08:43 -0500

    xvfb takes some time to start

--- dogtail-wrapper.sh
+++ dogtail-wrapper.sh
@@ -2,8 +2,11 @@
 
 set -xe
 
-Xvfb :1 -screen 0 1024x768x16 &> /var/log/xvfb.log &
+Xvfb :1 -screen 0 1024x768x16 &> /tmp/dogtail-root/xvfb.log &
 export DISPLAY=:1.0
+
+# xvfb takes some time to start
+sleep 0.5
 
 # Enable Assistive Technology support
 gsettings set org.gnome.desktop.interface toolkit-accessibility true
