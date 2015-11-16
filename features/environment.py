98b6e57122bec68ef94d0e0233c0b72076ace463
Tania Silva <tsilva@thoughtworks.com>
2015-11-16 17:03:48 -0500

    Using context instead of global var and killing coy after the test finish

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,15 @@
-import os
 from dogtail.procedural import *
+from psutil import process_iter
 
 def before_all(context):
-    pass
+  context.coy_pid = None
+  context.coyim_app = None
 
 def after_all(context):
-    pass
+  #context.coy_pid is shared fine between the steps but here is None
+  for process in process_iter():
+    for path in process.cmdline():
+      if "coyim" in path:
+        process.terminate()
+        exit()
 
--- features/steps/add_account.py
+++ features/steps/add_account.py
@@ -7,17 +7,7 @@ from dogtail.tc import TCNode, TCBool
 from dogtail.tree import *
 from dogtail.utils import screenshot
 
-coy_pid = None
 coy_config_file = "/tmp/coy-config.json"
-coyim_app = None
-tor_server = None
-
-class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-  pass
-
-class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
-  def handle(self):
-    pass
 
 @given(u'the user has no account configured')
 def step_impl(context):
@@ -26,27 +16,22 @@ def step_impl(context):
 
 @when(u'the user opens the application')
 def step_impl(context):
-  global coyim_app
-  global coy_pid
-
-  if coy_pid != None:
-    os.kill(coy_pid, signal.SIGTERM)
+  if context.coy_pid != None:
+    os.kill(context.coy_pid, signal.SIGTERM)
 
   coyimPath = os.environ.get('COYIM_PATH')
   if coyimPath != None:
     executable = "%s -debug -config-file %s" % (coyimPath, coy_config_file)
-    coy_pid = run(executable)
 
-  coyim_app = root.application("CoyIM")
+  context.coy_pid = run(executable)
+  context.coyim_app = root.application("CoyIM")
 
 @when(u'choses to not encrypt the configuration file')
 def step_impl(context):
-  global coyim_app
-  encryptAlert = coyim_app.child(name = "Question", roleName = "alert")
+  encryptAlert = context.coyim_app.child(name = "Question", roleName = "alert")
   encryptAlert.button("No").doActionNamed("click")
 
 @then(u'add account dialog is displayed')
 def step_impl(context):
-  global coyim_app
-  addAccount = coyim_app.child(name="Account Details", roleName = "dialog")
+  addAccount = context.coyim_app.child(name="Account Details", roleName = "dialog")
   TCNode().compare("Found add account dialog", None, addAccount)
